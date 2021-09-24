edaad13e38657c889b4a58fd8172155908efae15
J-Singh99 <jaspreet099@.com>
2021-09-24 13:15:45 +0530

    More shit sorted for GraphQL

--- schema.py
+++ schema.py
@@ -6,13 +6,15 @@ from graphene_django import DjangoObjectType
 # from trailers.models import Trailer, TrailerSafetyDetail, TrailerMonthlyDeduction, TrailerRepairService
 # from trucks.models import Truck, TruckSafetyDetail, TruckMonthlyDeduction
 from vendor.models import Vendor
-
+from trucks.models import Truck, TruckSafetyDetail, TruckMonthlyDeduction
+from drivers.models import Driver, MonthlyDeductionTable, DriverNote, DriverSafety
 
 ## QUERIES ##
 class VendorType(DjangoObjectType):
     class Meta:
         model = Vendor
         fields = (
+            "id",
             "user",
             "full_name",
             "address_street",
@@ -32,14 +34,75 @@ class VendorType(DjangoObjectType):
             "website",
             "notes" )
 
+
+class TruckType(DjangoObjectType):
+    class Meta:
+        model = Vendor
+        fields = (
+            "driver",
+            "unit",
+            "make",
+            "plate",
+            "type",
+            "year",
+            "driver_1",
+            "driver_2",
+            "country",
+            "state",
+            "VIN_number",
+            "terminal",
+            "city_truck",
+            "tour",
+            "still_working",
+            "leave_date",
+            "truck_ownership",
+            "value",
+            "weight_pounds",
+            "IFTA_group",
+            "account",
+            "percentage",
+            "rate_per_mile_LOAD",
+            "rate_per_mile_EMPTY",
+            "rate_per_hour")
+
+class TruckSafetyDetailType(DjangoObjectType):
+    class Meta:
+        model = Vendor
+        fields = (
+            "truck",
+            "safety_details",
+            "renewal_date",
+            "description",
+            "stop_dispatch_on_expiry")
+
+class TruckMonthlyDeductionType(DjangoObjectType):
+    class Meta:
+        model = Vendor
+        fields = (
+            "truck",
+            "monthly_deductions",
+            "day_of_month",
+            "currency_CDN",
+            "charges",
+            "valid_till",
+            "belongs_to_company",
+            "vendor",
+            "HST")
+
+
 class Query(graphene.ObjectType):
 
-    all_vendors = graphene.List(VendorType)
-    def resolve_all_vendors(root, info, f):
-        return Vendor.objects.all()
+    all_vendors = graphene.List(VendorType) #DjangoListField
+    def resolve_all_vendors(root, info):
+        return Vendor.objects.all() #Vendor.objects.filter(id=1)
 
 
+    vendor_with_id = graphene.Field(VendorType, id=graphene.Int())
+    def resolve_vendor_with_id(root, info, id):
+        return Vendor.objects.get(pk=id)
 
+    # resolve_whateevr:
+    #   whtevr.objects.filter(foreginKey = id)
 
 
 
