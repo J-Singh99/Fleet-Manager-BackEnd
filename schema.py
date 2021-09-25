2367e7bce19a9ed0faa0045281a3d38a1dd1b2c1
J-Singh99 <jaspreet099@.com>
2021-09-25 09:41:21 +0530

    Fixed querying by associated table

--- schema.py
+++ schema.py
@@ -89,6 +89,42 @@ class TruckMonthlyDeductionType(DjangoObjectType):
             "vendor",
             "HST")
 
+class DriverType(DjangoObjectType):
+    class Meta:
+        model = Driver
+        fields = (
+            "full_name",
+            "address_street",
+            "address_city",
+            "address_state",
+            "address_unit",
+            "address_ZIP",
+            "company",
+            "phone_int",
+            "cellphone_Canada",
+            "cellphone_US",
+            "WCB",
+            "SSN",
+            "driver_paid_by_Company_OwnerOperator",
+            "appointment_date",
+            "loadedMileRange_Alone",
+            "loadedMileRange_Team",
+            "emptyMileRange_Alone",
+            "emptyMileRange_Team",
+            "percentage_pay",
+            "perectage",
+            "notes",
+            "bank_details",
+            "truck_int",
+            "CSA_FAST_Driver",
+            "pay_GST",
+            "deduct_taxes_on_pay",
+            "exempt_EI",
+            "exempt_CPP",
+            "claim_code",
+            "CRA_Tax_pay_periods_year",
+            "currently_working")
+
 
 class Query(graphene.ObjectType):
 
@@ -109,8 +145,17 @@ class Query(graphene.ObjectType):
     def resolve_truck_with_id(root, info, id):
         return Truck.objects.get(pk=id)
 
-    # resolve_whateevr:
-    #   whtevr.objects.filter(foreginKey = id)
+    all_drivers = graphene.List(DriverType) #DjangoListField
+    def resolve_all_drivers(root, info):
+        return Driver.objects.all()
+
+    driver_with_id = graphene.Field(DriverType, id=graphene.Int())
+    def resolve_driver_with_id(root, info, id):
+        return Driver.objects.get(pk=id)
+
+    truck_by_driver = graphene.List(TruckType, id=graphene.Int())
+    def resolve_truck_by_driver(root, info, id):
+        return Truck.objects.filter(driver = id)
 
 
 
